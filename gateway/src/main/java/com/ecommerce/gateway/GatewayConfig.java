21db16dc4271ee58ceca9b681125e3fd1c4e21ac
mustofa adny <adniadni04@gmail.com>
2026-04-29 18:33:50 +0700

    fix: first hit service showing fallback circuit breaker

--- gateway/src/main/java/com/ecommerce/gateway/GatewayConfig.java
+++ gateway/src/main/java/com/ecommerce/gateway/GatewayConfig.java
@@ -5,6 +5,8 @@ import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+import java.time.Duration;
+
 @Configuration
 public class GatewayConfig {
 
@@ -13,21 +15,36 @@ public class GatewayConfig {
         return builder.routes()
                 .route("product-service", r -> r
                         .path("/api/products/**")
-                        .filters(f -> f.circuitBreaker(config -> config.setName("ecomBreaker")
-                                .setFallbackUri("forward:/fallback/products")))
-//                        .filters(f -> f.rewritePath("/products/(?<segment>/?.*)", "/api/products${segment}"))
+                        .filters(f -> f
+                                .retry(retry -> retry
+                                        .setRetries(1)
+                                        .setBackoff(Duration.ofMillis(100), Duration.ofMillis(500), 2, true)
+                                )
+                                .circuitBreaker(config -> config
+                                        .setName("ecomBreaker")
+                                        .setFallbackUri("forward:/fallback/products")))
                         .uri("lb://product-service"))
                 .route("user-service", r -> r
                         .path("/api/users/**")
-                        .filters(f -> f.circuitBreaker(config -> config.setName("ecomBreaker")
-                                .setFallbackUri("forward:/fallback/users")))
-//                        .filters(f -> f.rewritePath("/users/(?<segment>/?.*)", "/api/users{segment}"))
+                        .filters(f -> f
+                                .retry(retry -> retry
+                                        .setRetries(1)
+                                        .setBackoff(Duration.ofMillis(100), Duration.ofMillis(500), 2, true)
+                                )
+                                .circuitBreaker(config -> config
+                                        .setName("ecomBreaker")
+                                        .setFallbackUri("forward:/fallback/users")))
                         .uri("lb://user-service"))
                 .route("order-service", r -> r
-                        .path("/api/orders/**", "/cart/**")
-                        .filters(f -> f.circuitBreaker(config -> config.setName("ecomBreaker")
-                                .setFallbackUri("forward:/fallback/orders")))
-//                        .filters(f -> f.rewritePath("/(?<segment>.*)", "/api{segment}"))
+                        .path("/api/orders/**", "/api/cart/**")
+                        .filters(f -> f
+                                .retry(retry -> retry
+                                        .setRetries(1)
+                                        .setBackoff(Duration.ofMillis(100), Duration.ofMillis(500), 2, true)
+                                )
+                                .circuitBreaker(config -> config
+                                        .setName("ecomBreaker")
+                                        .setFallbackUri("forward:/fallback/orders")))
                         .uri("lb://order-service"))
                 .route("eureka-server", r -> r
                         .path("/eureka/main")
