1fa558ba9129d97c222e1682714b6c67c21e1d7a
mustofa adny <adniadni04@gmail.com>
2026-04-30 11:41:42 +0700

    consuming messages from rabbit mq

---
+++ notification/src/main/java/com/ecommerce/notification/OrderEventConsumer.java
@@ -0,0 +1,21 @@
+package com.ecommerce.notification;
+
+import org.springframework.amqp.rabbit.annotation.RabbitListener;
+import org.springframework.stereotype.Service;
+
+import java.util.Map;
+
+@Service
+public class OrderEventConsumer {
+    @RabbitListener(queues = "${rabbitmq.queue.name}")
+
+    public void handleOrderEvent(Map<String, Object> orderEvent) {
+        System.out.println("Received order event: " + orderEvent);
+
+        long orderId = Long.parseLong(orderEvent.get("orderId").toString());
+        String status = orderEvent.get("status").toString();
+
+        System.out.println("Oder ID: " + orderId);
+        System.out.println("Oder Status: " + status);
+    }
+}
